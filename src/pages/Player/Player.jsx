e51f3663b89911b00f0c6b77aa62162ed45e3180
Cindy Trissia Fania <cindytressia@gmail.com>
2025-12-29 23:26:27 +0700

    fixing navbar

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const Player = () => {
+
+  const {id} = useParams();
+  const navigate = useNavigate();
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    typeof: ""
+  })
+
+  const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNmI1NzIyNmJmZThkY2MzNTI0YjU3NzkyNjEwZDRlZiIsIm5iZiI6MTc2MjE2MjI5Ni4xOCwic3ViIjoiNjkwODc2NzhjOGZiZTIwNWU5NDY0NDRkIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.Noq1L8KYZH7j9kp7qtTJVbzPspdxvp63ruwsipl-WVM'
+  }
+};
+
+useEffect(()=>{
+  fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+  .then(res => res.json())
+  .then(res => setApiData(res.results[0]))
+  .catch(err => console.error(err));
+},[])
+
+
+
+
   return (
     <div className='player'>
-      
+      <img src={back_arrow_icon} alt="" onClick={() => {navigate(-2)}}/>
+      <iframe width='90%' height='90%'
+      src={`https://www.youtube.com/embed/${apiData.key}`}
+      title='trailer' frameBorder='0' allowFullScreen></iframe>
+      <div className='player-info'>
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
     </div>
   )
 }
